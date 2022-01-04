ea76b77b445e874a7e95257e1a2ce486efe4b078
Luis Cigler <luis@xian.at>
2022-01-04 11:57:50 +0100

    added checks for web download

--- fileget/Program.cs
+++ fileget/Program.cs
@@ -1,32 +1,73 @@
 ﻿using filgetlib;
 // See https://aka.ms/new-console-template for more information
+string version = "0.1.0";
 
-if (args[0] != null)
+if (args.Length > 0 )
 {
-    switch (args[0])
+    HandleCommands(args);
+} 
+else
+{
+    Console.WriteLine("Please enter a command and press ENTER:");
+    string[] arguments = Console.ReadLine().Split(' ');
+    HandleCommands(arguments);  
+}
+
+void PrintInfo()
+{
+    Console.WriteLine( "======================================");
+    Console.WriteLine( "| fileget Copyright (c) LuisTheOne   |");
+    Console.WriteLine($"| Version {version}                      |");
+    Console.WriteLine( "| github.com/LuisThe0ne/fileget      |");
+    Console.WriteLine( "======================================");
+}
+
+void PrintHelp()
+{
+    Console.WriteLine($"fileget V.{version} Help:");
+    Console.WriteLine("web remoteUrl localoutputPath");
+    Console.WriteLine("Visit the Github Repo for more Info (github.com/LuisThe0ne/fileget)");
+    Console.WriteLine("");
+    Console.WriteLine("Please enter a command and press ENTER:");
+    string[] arguments = Console.ReadLine().Split(' ');
+    HandleCommands(arguments);
+}
+
+void HandleCommands(string[] arguments)
+{
+    if (arguments.Length > 0)
     {
-        case "web":
-            Web web = new Web();
-            web.DownloadFile(args[1], args[2]);
-            break;
+        switch (arguments[0])
+        {
+            case "web":
+                if (arguments.Count() == 3)
+                {
+                    Web web = new Web();
+                    web.DownloadFile(arguments[1], arguments[2]);
+                } 
+                else
+                {
+                    Console.WriteLine("Invalid Arguments");
+                }               
+                break;
 
-        case "info":
+            case "info":
+                PrintInfo();
+                break;
 
-            PrintInfo();
-            break;
+            case "help":
+                PrintHelp();
+                break;
+
+            default:
+                PrintHelp();
+                break;
+        }
     }
-
-
-
-    Console.ReadKey();
-
-     void PrintInfo()
+    else 
     {
-        Console.WriteLine("=====================================");
-        Console.WriteLine("| fileget Copyright(c) LuisTheOne   |");
-        Console.WriteLine("| Version 1.0.0                     |");
-        Console.WriteLine("| github.com/LuisThe0ne/fileget     |");
-        Console.WriteLine("=====================================");
+        PrintHelp();
     }
+Console.ReadKey();
 }
 
--- filgetlib/Web.cs
+++ filgetlib/Web.cs
@@ -9,34 +9,84 @@ namespace filgetlib
 
         private int windowWidth = Console.WindowWidth;
         private int Percentage;
-        ConsoleProgressBar pb2 = new ConsoleProgressBar
-        {
-            NumberOfBlocks = (int)(Console.WindowWidth / 1.5),
-            ForegroundColor = ConsoleColor.White,
-            StartBracket = "[",
-            EndBracket = "]",
-            CompletedBlock = "#",
-            IncompleteBlock = "·",
-            AnimationSequence = UniversalProgressAnimations.Default,
-        };
+        ConsoleProgressBar pb2;
 
         public void DownloadFile(string inputurl, string outputpath)
         {
+            if (Uri.IsWellFormedUriString(inputurl, UriKind.Absolute))
+            {
+                Console.WriteLine("Valid Url");
+                HttpWebResponse response = null;
+                var request = (HttpWebRequest)WebRequest.Create(inputurl);
+                request.Method = "HEAD";
 
+                try
+                {
+                    response = (HttpWebResponse)request.GetResponse();
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    if (response != null)
+                    {
+                        Console.WriteLine("Url is reachable");
+                        response.Close();
+
+                        if (!string.IsNullOrEmpty(outputpath))
+                        {
+                            string outputFolder = Path.GetDirectoryName(outputpath);
+                            if (Directory.Exists(outputFolder))
+                            {
+                                Console.WriteLine("Output Path is valid");
+                                ExecuteDownloadFile(inputurl, outputpath);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Arguments");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid Arguments");
+                        }
+                    }
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Invalid Url");
+            }
+        }
+
+        private void ExecuteDownloadFile(string inputurl, string outputpath)
+        {
             Percentage = 0;
+
+            pb2 = new ConsoleProgressBar
+            {
+                NumberOfBlocks = (int)(Console.WindowWidth / 1.5),
+                ForegroundColor = ConsoleColor.White,
+                StartBracket = "[",
+                EndBracket = "]",
+                CompletedBlock = "#",
+                IncompleteBlock = "·",
+                AnimationSequence = UniversalProgressAnimations.Default,
+            };
+            Console.WriteLine("Starting Download");
             WebClient wc = new WebClient();
             wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(OnDownloadProgressChanged);
             wc.DownloadFileCompleted += new AsyncCompletedEventHandler(OnDownloadFileCompleted);
-            wc.DownloadFileAsync(new System.Uri (inputurl), outputpath);            
+            wc.DownloadFileAsync(new System.Uri(inputurl), outputpath);
         }
 
         private void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
             Percentage = e.ProgressPercentage;
-            if (Percentage != null)
-            {
-                pb2.Report((double)Percentage / 100);
-            }
+
+            pb2.Report((double)Percentage / 100);
         }
 
         private void OnDownloadFileCompleted(object? sender, AsyncCompletedEventArgs e)
