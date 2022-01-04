ffccc7f7c24f0afb89a28479c5051583be01aacf
Luis Cigler <luis@xian.at>
2022-01-04 09:29:30 +0100

    first working alpha

--- fileget/Program.cs
+++ fileget/Program.cs
@@ -10,13 +10,23 @@ if (args[0] != null)
             web.DownloadFile(args[1], args[2]);
             break;
 
-        case "debug":
+        case "info":
 
-            Web.DebugTest();
+            PrintInfo();
             break;
     }
 
 
+
     Console.ReadKey();
+
+     void PrintInfo()
+    {
+        Console.WriteLine("=====================================");
+        Console.WriteLine("| fileget Copyright(c) LuisTheOne   |");
+        Console.WriteLine("| Version 1.0.0                     |");
+        Console.WriteLine("| github.com/LuisThe0ne/fileget     |");
+        Console.WriteLine("=====================================");
+    }
 }
 
--- filgetlib/Web.cs
+++ filgetlib/Web.cs
@@ -1,5 +1,4 @@
-﻿using Konsole;
-using Luna.ConsoleProgressBar;
+﻿using Luna.ConsoleProgressBar;
 using System.ComponentModel;
 using System.Net;
 
@@ -7,10 +6,9 @@ namespace filgetlib
 {
     public class Web
     {
-        //private DustInTheWind.ConsoleTools.Spinners.ProgressBar? progressBar;
-        //ProgressBar pb = new ProgressBar(100);
+
         private int windowWidth = Console.WindowWidth;
-        private int myPercentage;
+        private int Percentage;
         ConsoleProgressBar pb2 = new ConsoleProgressBar
         {
             NumberOfBlocks = (int)(Console.WindowWidth / 1.5),
@@ -19,37 +17,26 @@ namespace filgetlib
             EndBracket = "]",
             CompletedBlock = "#",
             IncompleteBlock = "·",
-            AnimationSequence = UniversalProgressAnimations.Default
+            AnimationSequence = UniversalProgressAnimations.Default,
         };
 
         public void DownloadFile(string inputurl, string outputpath)
         {
-            //progressBar = new DustInTheWind.ConsoleTools.Spinners.ProgressBar();
-            //progressBar.EnsureBeginOfLine = true;
-            ////progressBar.Length = 10;
-            //progressBar.Display();
 
-            myPercentage = 0;
-
+            Percentage = 0;
             WebClient wc = new WebClient();
             wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(OnDownloadProgressChanged);
             wc.DownloadFileCompleted += new AsyncCompletedEventHandler(OnDownloadFileCompleted);
-
             wc.DownloadFileAsync(new System.Uri (inputurl), outputpath);            
         }
 
         private void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
-            myPercentage = e.ProgressPercentage;
-
-            //if ((progressBar != null) && (myPercentage % 10 == 0))
-            //{
-            //    progressBar.Value = e.ProgressPercentage;
-            //}
-            //Console.Write(e.ProgressPercentage);
-            pb2.Report((double)myPercentage / 100);
-            
-            //pb.Refresh(myPercentage, "Downloading...");
+            Percentage = e.ProgressPercentage;
+            if (Percentage != null)
+            {
+                pb2.Report((double)Percentage / 100);
+            }
         }
 
         private void OnDownloadFileCompleted(object? sender, AsyncCompletedEventArgs e)
@@ -65,32 +52,16 @@ namespace filgetlib
                 Console.WriteLine(e.Error.ToString());
                 return;
             }
-
-            //if (progressBar != null)
-            //{
-            //    progressBar.Close();                
-            //}
-            Task.Delay(1000);
-            Console.WriteLine("Download competed!");
+            
+            Task.Delay(5000);
+            pb2.Dispose();
+            Console.WriteLine("");
+            Console.WriteLine("Download completed!");
         }
 
         public static void DebugTest()
         {
-            DustInTheWind.ConsoleTools.Spinners.ProgressBar progressBar = new DustInTheWind.ConsoleTools.Spinners.ProgressBar();
 
-            Task.Run<Task>(async () =>
-            {
-                progressBar.Display();
-
-                for (int i = 0; i < 100; i++)
-                {
-                    await Task.Delay(100);
-                    progressBar.Value++;
-                }
-
-            });
-
-            progressBar.Close();
         }
     }
 }
